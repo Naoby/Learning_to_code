84fceb1ebe1feeafaefaca1c11f885ef4c0180a2
Naoby <augute.a@gmail.com>
2021-05-20 17:47:41 +0300

    Mu_files

---
+++ cars.py
@@ -0,0 +1,17 @@
+#the sort() Method
+cars = ['bmw', 'audi', 'toyota', 'subaru' ]
+cars.sort(reverse=True)
+print(cars)
+
+#Sorting a List Temporarily with the sorted() Function
+cars = ['bmw', 'audi', 'toyota', 'subaru' ]
+print("Here is original list:")   
+print(cars)
+
+print("\nHere's the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
+
+#a List in Reverse Order
